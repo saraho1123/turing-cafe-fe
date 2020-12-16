8427cc2ac808730af1a9c9e47d7302a6bdba6fdb
Sarah Osgood <sarahjs117@gmail.com>
2020-12-16 10:08:21 -0700

    Add render and methods to handle input changes and submit button

--- src/FormReservation/FormReservation.js
+++ src/FormReservation/FormReservation.js
@@ -2,16 +2,62 @@ import React, { Component } from 'react';
 import './FormReservation.css';
 
 class FormReservation extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-
+      id: null,
+      name: '',
+      date: '',
+      time: '',
+      number: '',
     }
+  }
+
+  changeHandler = (event) => {
+    this.setState({ [event.target.name]: event.target.value})
+  }
+
+  submitReservation = (event) => {
+    event.preventDefault();
+    const newReservation = {
+      id: Date.now(),
+      name: this.state.name,
+      date: this.state.date,
+      time: this.state.time,
+      number: this.state.number,
+    }
+    this.props.makeReservation(newReservation)
   }
 
   render() {
     return (
-      
+      <form className="reservation-form">
+        <input 
+          name="name"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={event => {this.changeHandler(event)}}
+        />
+        <input 
+          name="date"
+          placeholder="Date (mm/dd)"
+          value={this.state.date}
+          onChange={event => {this.changeHandler(event)}}
+        />       
+        <input 
+          name="time"
+          placeholder="Start Time  (hh:mm)"
+          value={this.state.time}
+          onChange={event => {this.changeHandler(event)}}
+        />        
+        <input 
+          name="number"
+          placeholder="Number of guests (max 4)"
+          value={this.state.number}
+          onChange={event => {this.changeHandler(event)}}
+        />
+        <button onClick={event => this.submitReservation(event)} className="make-reservation-button">Make Reservation</button>
+      </form>
     )
   }
 }
