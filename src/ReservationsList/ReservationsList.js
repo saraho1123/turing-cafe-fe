f479b90d1f2402affc0ca0a3d946450f912c43d0
Sarah Osgood <sarahjs117@gmail.com>
2020-12-16 09:31:13 -0700

    Set up ReservastionsList as a functional component and add a css file

---
+++ src/ReservationsList/ReservationsList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ReservationsList.css'
+
+const ReservationsList = () => {
+  return (
+    <h1>Reservations go here.</h1>
+  )
+}
+
+export default ReservationsList;
+
