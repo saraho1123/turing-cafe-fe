9120199a51a4efe468077315d0b63ab0904e613d
Sarah Osgood <sarahjs117@gmail.com>
2020-12-16 09:31:50 -0700

    Update App with imports of functional components and apiCalls

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import ReservationsList from '../ReservationsList/ReservationsList.js'
+import Reservation from '../Reservation/Reservation.js'
+import { getAllReservations } from '../apiCalls.js'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  
+
   render() {
     return (
-      <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
+      <main>
+        <div className="App">
+          <h1 className='app-title'>Turing Cafe Reservations</h1>
+          <div className='resy-form'>
+  
+          </div>
+          <div className='resy-container'>
+            
+          </div>
         </div>
-        <div className='resy-container'>
-          
-        </div>
-      </div>
+        <ReservationsList />
+      </main>
     )
   }
 }
