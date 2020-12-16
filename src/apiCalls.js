da060ccf2624e61b96004a11a2ab6a0157ca2166
Sarah Osgood <sarahjs117@gmail.com>
2020-12-16 11:54:43 -0700

    Add method to post new reservation

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getAllReservations = () =>  {
   return fetch('http://localhost:3001/api/v1/reservations')
     .then(response => response.json())
 }
+
+export const addNewReservation = (reservationInfo) => {
+  return fetch('http://localhost:3001/api/v1/reservations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reservationInfo),
+  })
+    .then(response => response.json())
+}
