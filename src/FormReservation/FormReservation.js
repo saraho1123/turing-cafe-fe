cd3373fde6c9f0ae099739390c43115f76ca0be1
Sarah Osgood <sarahjs117@gmail.com>
2020-12-16 09:56:28 -0700

    Set up FormReservation class component and added css file

---
+++ src/FormReservation/FormReservation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './FormReservation.css';
+
+class FormReservation extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default FormReservation;
